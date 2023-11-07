8afe95c56701e5e5ec89f97f990e2d9910020a9a
Walter Moleterno <135560995+Boilermaker74@users.noreply.github.com>
2023-11-07 17:45:00 -0600

    Update text.test.js

--- test/text.test.js
+++ test/text.test.js
@@ -1,10 +1,19 @@
 const {Text, TriangleText} = require("../lib/text")
 
-// Test for reqular text
+// Test for regular text
 
 test("hello black", ()  => {
     const text = new Text();
     text.characters = "hello";
     text.color = "black";
     expect(text.render()).toEqual(`<text x = "150" y = "115" text-anchor="middle" font-size="50" fill="black">hello</text>`)
+})
+
+// Test for TriangleText text
+
+test("hello black", ()  => {
+    const tritext = new TriangleText();
+    tritext.characters = "hello";
+    tritext.color = "black";
+    expect(tritext.render()).toEqual(`<text x = "150" y = "150" text-anchor="middle" font-size="50" fill="black">hello</text>`)
 })
